e2848224d66ab81db545fd9df01ed0a74d89b40f
xaviSalazar <xavicoel@gmail.com>
2022-01-18 21:47:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './App.css';
+import Home from './components/pages/Home.js';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home/>}/>
+      </Routes>
+    </Router>
+    </>
+
+   );
+}
+
+export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import HeroSection from '../HeroSection.js';
+
+
+
+function Home () {
+    return (
+        <>
+        <HeroSection />
+        </>
+    );
+}
+
+export default Home;

c74aeb0a0f0674619e31cea0bffa9a7beb141a97
xaviSalazar <xavicoel@gmail.com>
2022-01-19 23:42:44 +0100

    feat: sitio internet

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection.js';
+import Cards from '../Cards.js';
+import Footer from '../Footer';
 
 
 
@@ -8,6 +10,9 @@ function Home () {
     return (
         <>
         <HeroSection />
+        <Cards />
+        <Footer/>
+
         </>
     );
 }
